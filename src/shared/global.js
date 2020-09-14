050263c097d281578dfbd3ba996f484307dda64b
srulfi <santiagorulfi@gmail.com>
2020-09-14 11:18:26 -0300

    setup global style

---
+++ src/shared/global.js
@@ -0,0 +1,11 @@
+import { createGlobalStyle, css } from "styled-components"
+
+export const bodyStyles = css`
+	/* normalizer */
+`
+
+export const GlobalStyle = createGlobalStyle`
+ body {
+   ${bodyStyles}
+ }
+`
