b01f81dc8a7f310149d33bee68f8adfa32895cd7
srulfi <santiagorulfi@gmail.com>
2020-09-17 15:08:22 -0300

    move button styles outside component

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,40 +1,8 @@
 import React from "react"
 import PropTypes from "prop-types"
 import classNames from "classnames"
-import styled from "styled-components"
-import { Button as AntButton } from "antd"
 
-const StyledButton = styled(AntButton)`
-	&.quuack-button {
-		font-family: ${props => props.theme.fontBase};
-		border: 0;
-		border-radius: 3em;
-		cursor: pointer;
-		display: inline-block;
-		line-height: 1;
-	}
-	&.quuack-button--primary {
-		background-color: ${props => props.theme.colorPrimary};
-		color: ${props => props.theme.textInverseColor};
-	}
-	&.quuack-button--secondary {
-		color: ${props => props.theme.colorSecondary};
-		background-color: transparent;
-		box-shadow: rgba(0, 0, 0, 0.15) 0px 0px 0px 1px inset;
-	}
-	&.quuack-button--small {
-		font-size: 12px;
-		padding: 10px 16px;
-	}
-	&.quuack-button--medium {
-		font-size: 14px;
-		padding: 11px 20px;
-	}
-	&.quuack-button--large {
-		font-size: 16px;
-		padding: 12px 24px;
-	}
-`
+import StyledButton from "./Button.styles"
 
 /**
  * Primary UI component for user interaction
