7eb966c985585eacc71db5aa4f93a73e93db5bcc
Santiago Rulfi <santiagorulfi@gmail.com>
2020-09-21 19:10:58 -0300

    version dist files, add eslintignore

---
+++ dist/components/Examples/Page.stories.js
@@ -0,0 +1,17 @@
+import _objectSpread from "@babel/runtime/helpers/esm/objectSpread2"
+import React from "react"
+import { Page } from "./Page"
+import * as HeaderStories from "./Header.stories"
+export default {
+	title: "Example/Page",
+	component: Page,
+}
+
+var Template = function Template(args) {
+	return /*#__PURE__*/ React.createElement(Page, args)
+}
+
+export var LoggedIn = Template.bind({})
+LoggedIn.args = _objectSpread({}, HeaderStories.LoggedIn.args)
+export var LoggedOut = Template.bind({})
+LoggedOut.args = _objectSpread({}, HeaderStories.LoggedOut.args)
---
+++ dist/index.js
@@ -0,0 +1,3 @@
+import * as global from "./shared/global"
+export { global }
+export { default as Button } from "./components/Button"
