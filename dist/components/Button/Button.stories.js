7bf30570135136e348004fe15b709b263daf0139
srulfi <srulfi@users.noreply.github.com>
2020-09-29 19:32:05 +0000

    build dist

--- dist/components/Button/Button.stories.js
+++ dist/components/Button/Button.stories.js
@@ -1,7 +1,7 @@
 import React from "react";
 import Button from "./Button";
 export default {
-  title: "Design System/Button",
+  title: "Components/Button",
   component: Button,
   argTypes: {
     backgroundColor: {
