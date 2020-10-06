6cdeb0525044df83ea1880ab71c42c67ab419599
Santiago Rulfi <santiagorulfi@gmail.com>
2020-10-06 13:16:42 -0300

    remnove comments from index

--- src/index.js
+++ src/index.js
@@ -1,10 +1,7 @@
-// global
 import GlobalStyle from "./shared/global"
 
-// components
 import Button from "./components/Button"
 
-// hooks
 import useWindowSize from "./hooks/useWindowSize"
 
 export { GlobalStyle, Button, useWindowSize }
