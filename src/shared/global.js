9d01be0f5a2db0e3ffed2b44c4e85ed68a4a0566
Santiago Rulfi <santiagorulfi@gmail.com>
2020-10-13 13:20:29 -0300

    import styles in theme, global and button

--- src/shared/global.js
+++ src/shared/global.js
@@ -1,8 +1,12 @@
 import { createGlobalStyle, css } from "styled-components"
 import { normalize } from "styled-normalize"
 
+import { colors, typography } from "./styles"
+
 const bodyStyles = css`
-	/* additional body styles */
+	font-family: ${typography.type.primary};
+	font-size: ${typography.size.s3}px;
+	color: ${colors.primary};
 `
 
 const GlobalStyle = createGlobalStyle`
